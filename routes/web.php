ccde5ed59f1cfb873af4358c022abb6411081b22
lkey <lkey_email@163.com>
2019-10-15 03:39:10 +0000

    创建会话

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('help', 'StaticPagesController@help')->name('help');
 Route::get('about', 'StaticPagesController@about')->name('about');
@@ -22,3 +18,7 @@ Route::get('about', 'StaticPagesController@about')->name('about');
 Route::get('signup', 'UsersController@create')->name('signup');
 
 Route::resource('users', 'UsersController');
+
+Route::get('login', 'SessionsController@create')->name('login');
+Route::post('login', 'SessionsController@store')->name('login');
+Route::delete('logout', 'SessionsController@destroy')->name('logout');
